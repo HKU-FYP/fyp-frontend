744647d67795414765a325c8e0372288e04cf2d7
KIM TAEHYUN <tanggu01@connect.hku.hk>
2025-03-05 22:32:49 +0800

    connect news detail API

--- src/pages/dashboard/components/NewsDetail.jsx
+++ src/pages/dashboard/components/NewsDetail.jsx
@@ -5,6 +5,10 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {Typography, Box} from "@mui/material";
 import {newsData} from "./MainGrid.jsx";
+import {getNewsDetailByNewsId} from "../../../api/news.js";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
 
 export default function NewsDetail() {
 
@@ -41,8 +45,9 @@ export default function NewsDetail() {
     const [newsDetail, setNewsDetail] = useState(null);
 
     useEffect(() => {
-        const detail = newsData.find(item => item.id === parseInt(id));
-        setNewsDetail(detail);
+        getNewsDetailByNewsId(id)
+            .then((data) => setNewsDetail(data))
+            .catch((error) => console.error("Failed to fetch news detail:", error));
     }, [id]);
 
     if (!newsDetail) {
@@ -56,8 +61,8 @@ export default function NewsDetail() {
     return (
         <Box sx={{padding: '20px'}}>
 
-            <Stack spacing={5}>
-                <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+            <Stack spacing={1.5}>
+                <Typography variant="h4" sx={{fontWeight: 'bold'}}>
                     {newsDetail.title}
                 </Typography>
 
@@ -65,8 +70,30 @@ export default function NewsDetail() {
                 <Typography variant="body1">{newsDetail.summary}</Typography>
                 {/*<Skeleton variant="rectangular" height={200}/>*/}
 
+                {/*<h2> Content</h2>*/}
+                {/*<Typography variant="body1">{newsDetail.content}</Typography>*/}
+
+
+                <h2>Key Metrics</h2>
+                <List>
+                    {newsDetail.key_metrics.map((metric, index) => (
+                        <ListItem key={index}>
+                            <ListItemText primary={metric}/>
+                        </ListItem>
+                    ))}
+                </List>
+
                 <h2> Stock impact analysis </h2>
-                <Typography variant="body1">{newsDetail.fullanalysis}</Typography>
+
+                <Typography variant="body1">Overall Sentiment: {newsDetail.sentiment}</Typography>
+                <Typography variant="body1">{newsDetail.sentiment_analysis}</Typography>
+
+                {/*<Typography variant="body1">{newsDetail.fullanalysis}</Typography>*/}
+                <Typography variant="body1">
+                    {selectedButton === 'Beginner' && (newsDetail.stock_impact_analysis_easy || fullanalysis)}
+                    {selectedButton === 'Intermediate' && (newsDetail.stock_impact_analysis_intermediate || fullanalysis)}
+                    {selectedButton === 'Expert' && (newsDetail.stock_impact_analysis_expert || fullanalysis)}
+                </Typography>
                 {/*<Skeleton variant="rounded" height={300}/>*/}
                 <Stack spacing={2} direction="row">
                     <Button
